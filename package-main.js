d950a4434b295d3d0bcd1c51b53648e1c8bd6a28
lukakoning <koningluka@gmail.com>
2026-04-03 19:55:26 +0200

    Update package-main.js

--- package-main.js
+++ package-main.js
@@ -232,7 +232,7 @@ function createSplashWindow() {
         </style>
       </head>
       <body>
-        <h1>App is loading...</h2>
+        <h1>App is loading...</h1>
         <p>This should take no longer than 30 seconds.<br>
         (The first time may take longer, as it needs to install Python.)<br>
         Is it not working? View the log file:<br><br>
