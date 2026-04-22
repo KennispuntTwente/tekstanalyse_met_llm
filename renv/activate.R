0e7abbd9396c524a6a6ee458c7a49e3cfe8de3c1
lukakoning <koningluka@gmail.com>
2026-04-22 21:11:55 +0200

    Upgrade renv

--- renv/activate.R
+++ renv/activate.R
@@ -2,8 +2,8 @@
 local({
 
   # the requested version of renv
-  version <- "1.2.0"
-  attr(version, "md5") <- "55a999ab1366249f2dd6c52e0d2489f6"
+  version <- "1.2.2"
+  attr(version, "md5") <- "02a5732a42a5a912a107dfdc37f7c20d"
   attr(version, "sha") <- NULL
 
   # the project directory
